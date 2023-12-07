20bc3f9374437d843ae2597e9d15ae32b0ab6ba4
Yorkie Makoto <yorkiefixer@gmail.com>
2023-12-08 03:34:42 +0800

    jest: use js-with-ts-esm preset

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   collectCoverageFrom: [
     'src/**/*.ts',
