689788ef32055feea8efb6b5618c2dc7fbde5569
Yorkie Makoto <yorkiefixer@gmail.com>
2023-12-26 11:10:23 +0800

    morphtarget: use spatial css for example

--- fixtures/spatial-morph-target.ts
+++ fixtures/spatial-morph-target.ts
@@ -1,20 +1,14 @@
 const scene = spatialDocument.scene;
 const sphereElement = spatialDocument.querySelector('sphere');
-const mat = new BABYLON.StandardMaterial('mat', scene);
-mat.diffuseTexture = new BABYLON.Texture('https://playground.babylonjs.com/textures/misc.jpg', scene);
 
 const sphere = sphereElement.asNativeType();
-sphere.material = mat;
-
-function scrambleUp(data) {
+const sphere2 = sphere.clone('sphere2');
+sphere2.setEnabled(false);
+sphere2.updateMeshPositions(function scrambleUp(data) {
   for (let index = 0; index < data.length; index++) {
     data[index] += 0.4 * Math.random();
   }
-}
-
-const sphere2 = sphere.clone('sphere2');
-sphere2.setEnabled(false);
-sphere2.updateMeshPositions(scrambleUp);
+});
 
 const manager = new BABYLON.MorphTargetManager();
 sphere.morphTargetManager = manager;
