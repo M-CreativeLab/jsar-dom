339f3bd880558f214ee8022c37b9875aafc25b61
Yorkie Makoto <yorkiefixer@gmail.com>
2024-01-17 19:09:34 +0800

    update external example

--- fixtures/spatial-externalmesh-glb.ts
+++ fixtures/spatial-externalmesh-glb.ts
@@ -1,1 +1,9 @@
-// TODO
+spatialDocument.addEventListener('spaceReady', () => {
+  const scene = spatialDocument.scene;
+  const animations = scene.animationGroups
+    .filter(ag => ag.name.startsWith('model.'));
+
+  if (animations.length > 0) {
+    animations[0].start(true);
+  }
+});
