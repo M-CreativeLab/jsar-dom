f53fe72041cb937dd9ef77e87d0f9701510c83d3
neverbiasu <1751162157@qq.com>
2024-05-17 12:16:31 +0800

    fixBug

--- src/living/helpers/babylonjs/InteractiveDynamicTexture.test.ts
+++ src/living/helpers/babylonjs/InteractiveDynamicTexture.test.ts
@@ -1,19 +1,16 @@
 import { InteractiveDynamicTexture } from './InteractiveDynamicTexture';
 import DOMMatrix from '../../geometry/DOMMatrix'
 import { describe, it, expect } from '@jest/globals'
-import { loadImplementations } from '../../interfaces'
 
 describe('InteractiveDynamicTexture', () => {
-  it('should return correct matrix', async () => {
-    await loadImplementations();
+  it('should return correct matrix', () => {
     const transformStr = "rotate(90deg) translateX(10px)"
     const expectedMatrix = new DOMMatrix([0, 1, 0, 0,   -1, 0, 0, 0,  0, 0, 1, 0,  0, 10, 0, 1]);
     const result = InteractiveDynamicTexture._parserTransform(transformStr);
 
     expect(result).toEqual(expectedMatrix);
   })
-  it('should return identity matrix when no transform is applied', async () => {
-    await loadImplementations();
+  it('should return identity matrix when no transform is applied', () => {
     const transformStr = " "
     const expectedMatrix = new DOMMatrix([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
     const result = InteractiveDynamicTexture._parserTransform(transformStr);
